baa2899337bafe9bc4123a1335fa538203fbee41
Juan Mireles <119257668+Kal0yanX@users.noreply.github.com>
2023-08-20 10:23:44 -0400

    works up until Creating the Facts Index Template

--- petfax/fact.py
+++ petfax/fact.py
@@ -1,7 +1,13 @@
-from flask import Blueprint, render_template
+from flask import ( Blueprint, render_template, request, redirect ) 
 
-bp = Blueprint("fact", __name__, url_prefix="/facts")
+bp = Blueprint('fact', __name__, url_prefix="/facts")
 
+@bp.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        print(request.form)
+        return 'Thanks for submitting a fun fact!'
+    return 'This is the facts index'
 
 @bp.route("/new")
 def fact():
